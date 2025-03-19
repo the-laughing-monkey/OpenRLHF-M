ae5c07de6543e6b05ae1380ffd1007cf1c515a25
the-laughing-monkey <18448543+the-laughing-monkey@users.noreply.github.com>
2025-03-19 10:51:27 +0100

    fix to single node script

--- examples/scripts/tests/train_grpo_ray_qwen2_5_vl_mathv60k.sh
+++ examples/scripts/tests/train_grpo_ray_qwen2_5_vl_mathv60k.sh
@@ -156,7 +156,7 @@ ray job submit --address="http://127.0.0.1:8265" \
    --actor_num_nodes 1 \
    --actor_num_gpus_per_node 4 \
    --vllm_num_engines 2 \
-   --vllm_tensor_parallel_size 4 \
+   --vllm_tensor_parallel_size 2 \
    --colocate_all_models \
    --vllm_enable_sleep \
    --vllm_gpu_memory_utilization 0.5 \
