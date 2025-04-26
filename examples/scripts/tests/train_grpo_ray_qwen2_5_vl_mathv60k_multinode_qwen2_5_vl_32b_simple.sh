6a11dbdcf98812e072eeb8f9c2c04531daf328e1
the-laughing-monkey <18448543+the-laughing-monkey@users.noreply.github.com>
2025-04-26 17:39:29 +0200

    one more try at 32B working config across two nodes

--- examples/scripts/tests/train_grpo_ray_qwen2_5_vl_mathv60k_multinode_qwen2_5_vl_32b_simple.sh
+++ examples/scripts/tests/train_grpo_ray_qwen2_5_vl_mathv60k_multinode_qwen2_5_vl_32b_simple.sh
@@ -51,6 +51,7 @@ echo "Using eth1 IP address: ${ETH1_IP}"
          --vllm_tensor_parallel_size 4 \
          --vllm_enable_sleep \
          --vllm_sync_backend nccl \
+         --vllm_gpu_memory_utilization 0.6 \
          --enable_prefix_caching \
          --pretrain ${PRETRAIN_MODEL_PATH} \
          --save_path ${SAVE_PATH}/${MODEL_NAME} \
