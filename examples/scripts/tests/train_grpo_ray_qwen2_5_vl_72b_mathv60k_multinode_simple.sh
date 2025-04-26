b5156eeef985e1d700f99860ffb8917a7fdf80a6
the-laughing-monkey <18448543+the-laughing-monkey@users.noreply.github.com>
2025-04-26 17:09:59 +0200

    Update to 72B model script

--- examples/scripts/tests/train_grpo_ray_qwen2_5_vl_72b_mathv60k_multinode_simple.sh
+++ examples/scripts/tests/train_grpo_ray_qwen2_5_vl_72b_mathv60k_multinode_simple.sh
@@ -46,8 +46,8 @@ echo "Using eth1 IP address: ${ETH1_IP}"
          --remote_rm_url http://${ETH1_IP}:5000/get_reward \
          --actor_num_nodes 1 \
          --actor_num_gpus_per_node 32 \
-         --vllm_num_engines 8 \
-         --vllm_tensor_parallel_size 4 \
+         --vllm_num_engines 4 \
+         --vllm_tensor_parallel_size 8 \
          --vllm_enable_sleep \
          --vllm_sync_backend nccl \
          --vllm_gpu_memory_utilization 0.4 \
