6d2838c95ea65fe18f2cf8764affc4c8f925f391
the-laughing-monkey <18448543+the-laughing-monkey@users.noreply.github.com>
2025-04-29 14:46:48 +0200

    1 x 16; colocate actor and ref model

--- examples/scripts/tests/train_grpo_ray_qwen2_5_vl_32b_mathv60k_multinode_simple.sh
+++ examples/scripts/tests/train_grpo_ray_qwen2_5_vl_32b_mathv60k_multinode_simple.sh
@@ -43,15 +43,16 @@ echo "Using eth1 IP address: ${ETH1_IP}"
      --runtime-env-json="{\"working_dir\": \"${WORKSPACE_DIR}\"}" \
      -- python3 -m openrlhf.cli.train_ppo_ray \
          --ref_num_nodes 1 \
-         --ref_num_gpus_per_node 8 \
+         --ref_num_gpus_per_node 16 \
          --remote_rm_url http://${ETH1_IP}:5000/get_reward \
          --actor_num_nodes 1 \
-         --actor_num_gpus_per_node 8 \
+         --actor_num_gpus_per_node 16 \
+         --colocate_actor_ref \
          --vllm_num_engines 2 \
-         --vllm_tensor_parallel_size 4 \
+         --vllm_tensor_parallel_size 8 \
          --vllm_enable_sleep \
          --vllm_sync_backend nccl \
-         --vllm_gpu_memory_utilization 0.6 \
+         --vllm_gpu_memory_utilization 0.5 \
          --enable_prefix_caching \
          --pretrain ${PRETRAIN_MODEL_PATH} \
          --save_path ${SAVE_PATH}/${MODEL_NAME} \
